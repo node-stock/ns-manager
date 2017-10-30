b6be8e084c8654316fe421081438f4f8a33d946a
zlq4863947 <zlq4863947@gmail.com>
2017-10-31 04:44:27 +0900

    代码更新

--- config/init.sql
+++ config/init.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS `Signal` (
+CREATE TABLE IF NOT EXISTS `signal` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `symbol` varchar(20) DEFAULT NULL,
   `timeframe` varchar(10) DEFAULT NULL,
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS `Signal` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='信号';
 
-CREATE TABLE `Account` (
+CREATE TABLE `account` (
   `id` varchar(20) NOT NULL,
   `balance` bigint(20) DEFAULT NULL,
   `created_at` varchar(255) DEFAULT NULL,
@@ -20,7 +20,7 @@ CREATE TABLE `Account` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='账户';
 
-CREATE TABLE `Position` (
+CREATE TABLE `position` (
   `id` int(20) NOT NULL AUTO_INCREMENT,
   `account_id` varchar(255) DEFAULT NULL,
   `symbol` varchar(20) DEFAULT NULL,
@@ -33,10 +33,10 @@ CREATE TABLE `Position` (
   `deleted_at` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `account_id` (`account_id`),
-  CONSTRAINT `position_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `Account` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `position_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='持仓';
 
-CREATE TABLE `Transaction` (
+CREATE TABLE `transaction` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `account_id` varchar(255) DEFAULT NULL,
   `order` varchar(20) DEFAULT NULL,
@@ -49,5 +49,5 @@ CREATE TABLE `Transaction` (
   `deleted_at` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `account_id` (`account_id`),
-  CONSTRAINT `transaction_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `Account` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `transaction_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COMMENT='交易记录';
