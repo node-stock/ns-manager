050f6114acc92d33abf3f7f0a084cd5e9d22f269
zlq4863947 <zlq4863947@gmail.com>
2017-10-31 04:29:15 +0900

    增加交易管理器

--- src/lib/manager.test.ts
+++ src/lib/manager.test.ts
@@ -1,5 +1,5 @@
 import * as assert from 'power-assert';
-import { OrderSide } from 'ns-types';
+import { OrderSide, LimitOrder, OrderType, TradeType, EventType } from 'ns-types';
 import { Manager } from './manager';
 
 const manager = new Manager();
@@ -11,7 +11,7 @@ const testSetSignal = async (done: any) => {
     price: 2000,
     timeframe: '5min',
     notes: '备注项目'
-  })
+  });
   console.log(res);
   assert(res);
   done();
@@ -43,9 +43,58 @@ const testRemoveSignal = async (done: any) => {
   const res = await manager.signal.remove(signal.id);
   console.log(res);
   assert(res);
-  manager.destroy();
   done();
 }
+
+const testGetBalance = async (done: any) => {
+
+  const balance = await manager.asset.getBalance('test');
+  console.log(balance);
+  assert(true);
+  const balance2 = await manager.asset.getBalance('testxxx');
+  console.log(balance2);
+  assert(balance2 === 0);
+  done();
+}
+
+const testBuyTrader = async (done: any) => {
+  const order: LimitOrder = {
+    symbol: '6664',
+    side: OrderSide.Buy,
+    orderType: OrderType.Limit,
+    tradeType: TradeType.Margin,
+    eventType: EventType.Order,
+    price: 2000,
+    amount: 100
+  };
+  const account = {
+    id: 'test',
+    balance: 300000
+  }
+  await manager.trader.set(account, order);
+  assert(true);
+  done();
+}
+
+const testSellTrader = async (done: any) => {
+  const order: LimitOrder = {
+    symbol: '6664',
+    side: OrderSide.Sell,
+    orderType: OrderType.Limit,
+    tradeType: TradeType.Margin,
+    eventType: EventType.Order,
+    price: 2100,
+    amount: 100
+  };
+  const account = {
+    id: 'test',
+    balance: await manager.asset.getBalance('test')
+  }
+  await manager.trader.set(account, order);
+  assert(true);
+  done();
+}
+
 
 describe('ns-manager', () => {
   it('存储信号', function (done) {
@@ -60,4 +109,19 @@ describe('ns-manager', () => {
     this.timeout(20000);
     testRemoveSignal(done);
   });
+  it('获取余额', function (done) {
+    this.timeout(20000);
+    testGetBalance(done);
+  });
+  it('记录买单交易', function (done) {
+    this.timeout(20000);
+    testBuyTrader(done);
+  });
+  it('记录卖单交易', function (done) {
+    this.timeout(20000);
+    testSellTrader(done);
+  });
+  after(() => {
+    manager.destroy();
+  })
 });
